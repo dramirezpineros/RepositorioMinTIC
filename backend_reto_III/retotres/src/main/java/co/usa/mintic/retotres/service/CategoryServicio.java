813cdf3c17b5bbbc148e0c97ff86c82b0fde6401
dramirezpineros <92070288+dramirezpineros@users.noreply.github.com>
2021-10-23 09:14:08 -0500

    Add files via upload

---
+++ backend_reto_III/retotres/src/main/java/co/usa/mintic/retotres/service/CategoryServicio.java
@@ -0,0 +1,39 @@
+package co.usa.mintic.retotres.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import co.usa.mintic.retotres.model.Category;
+import co.usa.mintic.retotres.repository.CategoryRepositorio;
+
+@Service
+public class CategoryServicio {
+    @Autowired
+    private CategoryRepositorio categoryRepositorio;
+
+    public List<Category>getAll(){
+        return categoryRepositorio.getAll();
+    }
+    
+
+    public Optional<Category>getCategory(int id){
+        return categoryRepositorio.getCategory(id);
+    }
+    
+
+    public Category save(Category categoria){
+        if(categoria.getId()==null){
+            return categoryRepositorio.save(categoria);
+        }else{
+            Optional<Category> consulta= categoryRepositorio.getCategory(categoria.getId());
+            if(consulta.isEmpty()){
+                return categoryRepositorio.save(categoria);
+            }else{
+                return categoria;
+            }
+        }
+    }
+}
